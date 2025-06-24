db8706c4481dd7eae596f98cf7c7355d2dccea49
Luiz Felipe Osti <lfelipe.osti@gmail.com>
2025-06-24 20:56:24 -0300

    Added Spring Project

---
+++ src/main/java/PGO/PgoApplication.java
@@ -0,0 +1,13 @@
+package PGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PGO/PgoApplicationTests.java
@@ -0,0 +1,13 @@
+package PGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
